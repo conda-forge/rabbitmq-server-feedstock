7f785acf16fc1dc5bb66effa71822a6317cf470b
Patrick Sodré <sodre@sodre.co>
2018-03-06 18:55:40 -0500

    Use conda-build=3 because of Erlang issue

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -57,7 +57,9 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
+conda install --yes conda-build=3
 conda build /recipe_root --quiet || exit 1
+conda install --yes conda-build=2
 upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
 touch /feedstock_root/build_artefacts/conda-forge-build-done
